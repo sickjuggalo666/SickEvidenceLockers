0dd7d179950941752518f7c699bc831b14478b53
SickJuggalo666 <82001106+sickjuggalo666@users.noreply.github.com>
2023-02-25 16:57:27 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ name "SickEvidence"
 description "A locker/evidence storage using ox_libs and inventory"
 author "SickJuggalo666"
 version "1.0.0"
-
+lua54 'yes'
 shared_scripts {
     '@ox_lib/init.lua',
 	'@es_extended/locale.lua',
