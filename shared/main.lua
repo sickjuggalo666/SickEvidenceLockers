efb6c8252fa761d642efc8e548eebf38244e1140
SickJuggalo666 <82001106+sickjuggalo666@users.noreply.github.com>
2023-03-01 23:05:13 -0700

    just for the sake of matching

--- shared/main.lua
+++ shared/main.lua
@@ -9,9 +9,9 @@ Config.location = {
         h = 162.1413, 
         size = vec3(3, 2, 3), -- size of the box zone
         rotation = 90, -- Rotation of box zone
-        AllowedRank = 3, -- allowed ranks for Chief Options
         cop = true,  -- is this a police job? allows evidence lockers
         job = 'police', -- what job do you want here?
+        AllowedRank = 3, -- allowed ranks for Chief Options
         TargetLabel = 'Open Evidence', -- easier to label for each job
         ped = 's_m_m_armoured_01' -- ped is now location/job based
     },
@@ -21,9 +21,9 @@ Config.location = {
         h = 72.4052, 
         size = vec3(3, 2, 3), -- size of the box zone
         rotation = 90, -- Rotation of box zone
-        AllowedRank = 3, 
         cop = false,
         job = 'ambulance',
+        AllowedRank = 3,
         TargetLabel = 'Open Ambulance Lockers',
         ped = 'S_M_M_Doctor_01'
     }
